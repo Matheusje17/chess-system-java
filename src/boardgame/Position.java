6605311bda12b04899ca43c678461a4c997c6060
Matheus de Jesus <matheusje17@gmail.com>
2021-07-19 17:16:56 -0300

    Primeira classe Position

---
+++ src/boardgame/Position.java
@@ -0,0 +1,17 @@
+package boardgame;
+
+public class Position {
+	private int row;
+	private int column;
+	
+	public Position(int row, int column) {
+		super();
+		this.row = row;
+		this.column = column;
+	}
+	
+	public Position() {
+		
+	}
+	
+}
